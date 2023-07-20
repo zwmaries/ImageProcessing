3ac1fbdec1505e1c74ac8467f86f7bf8daab25bd
xiaoming <@1844007617.com>
2023-07-20 12:46:07 +0800

    视频读取2

--- 视频读取/视频读取.cpp
+++ 视频读取/视频读取.cpp
@@ -5,20 +5,20 @@ using namespace cv;
 
 void video_demo(Mat& image)
 {
-	VideoCapture capture(0);
+	//VideoCapture capture(0);
 	//读取指定视频
-	/*VideoCapture capture("C:/Users/pc-/Desktop/WeChat_20230719100517.mp4");
+	VideoCapture capture("C:/Users/pc-/Desktop/WeChat_20230719100517.mp4");
 	
-	int frame_width = capture.get(CAP_PROP_FRAME_WIDTH);
-	int frame_height = capture.get(CAP_PROP_FRAME_HEIGHT);
-	int count = capture.get(CAP_PROP_FRAME_COUNT);
-	double fps = capture.get(CAP_PROP_FPS);
+	int frame_width = capture.get(CAP_PROP_FRAME_WIDTH);//3
+	int frame_height = capture.get(CAP_PROP_FRAME_HEIGHT);//4
+	int count = capture.get(CAP_PROP_FRAME_COUNT);//5
+	double fps = capture.get(CAP_PROP_FPS);//7
 	cout << "frame width:" << frame_width << endl;
 	cout << "frame height:" << frame_height << endl;
 	cout << "Number of Frames:" << count << endl;
 	cout << "fps:" << fps << endl;
 	//写入test.mp4中
-	VideoWriter writer("C:/Users/pc-/Desktop/test.mp4", capture.get(CAP_PROP_FOURCC), fps, Size(frame_width, frame_height), true);*/
+	VideoWriter writer("C:/Users/pc-/Desktop/test.mp4", capture.get(CAP_PROP_FOURCC), fps, Size(frame_width, frame_height), true);
 	Mat frame;
 	while (true)
 	{
